c2ae9ee6e7deb0f8f31efe0beb4d9b16d69109a8
ludollaoo <ludoduplessis@hotmail.fr>
2020-11-03 18:03:23 +0100

    debut de la branche sigma_delta

--- Code/main.c
+++ Code/main.c
@@ -5,6 +5,7 @@
 #include "nrutil.h"
 
 #define threshold 10
+#define nb_img_max 200
 
 
  void frame_difference(){
@@ -46,10 +47,62 @@
 	}
  }
 
+ void get_path(int numero_image, char * path){
+ 	sprintf(path, "/home/ludovic/HPC/Projet/car3/car_3%03d.pgm", numero_image);
+ }
+
 
 
 int main(void){
-	frame_difference();
+	//taille des images
+	const int nrl = 0;
+	const int nrh = 239;
+	const int ncl = 0;
+	const int nch = 319;
+
+	//chemin des images
+	char path[100];
+
+	//////////////////////////////
+	//calcul de la """moyenne"""//
+	//////////////////////////////
+
+	//creation et chargement de l'image moyenne
+	uint8** moyenne = ui8matrix(nrl, nrh, ncl, nch);
+	get_path(0, path);
+	MLoadPGM_ui8matrix(path, nrl, nrh, ncl, nch, moyenne);
+	uint8 pixel_moyenne;
+
+	//creation de l'image courante
+	uint8** image_courante = ui8matrix(nrl, nrh, ncl, nch);
+	uint8 pixel_courant;
+
+	//parcours des 200 images
+	for(int n_img = 1; n_img < nb_img_max; n_img++){
+
+		//chargement de l'image courante
+		get_path(n_img, path);
+		MLoadPGM_ui8matrix(path, nrl, nrh, ncl, nch, image_courante);
+
+		//parcours de l'image courante et incrementation/decrementation de l'image moyenne
+		for(int i = nrl; i <= nrh; i++){
+			for(int j = ncl; j <= nch; j++){
+				pixel_moyenne = moyenne[i][j];
+				pixel_courant = image_courante[i][j];
+				if(pixel_moyenne > pixel_courant){
+					moyenne[i][j] -= 1;
+				}else if(pixel_moyenne < pixel_courant){
+					moyenne[i][j] += 1;
+				}
+			}
+		}
+	}
+
+	//visualisation de la moyenne
+	//SavePGM_ui8matrix(moyenne, nrl, nrh, ncl, nch,"/home/ludovic/HPC/Projet/Code/image_test");
+
+	free_ui8matrix(moyenne, nrl, nrh, ncl, nch);
+	free_ui8matrix(image_courante, nrl, nrh, ncl, nch);
 }
 
 	// exemple de fonction pour ouverture et fermeture, attention au chemin
