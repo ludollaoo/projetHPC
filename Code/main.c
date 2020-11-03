b85939cc66a0107f848cd24cc86aaad7c89a09bf
ludollaoo <ludoduplessis@hotmail.fr>
2020-11-03 15:23:40 +0100

    pour afif

--- Code/main.c
+++ Code/main.c
@@ -1,11 +1,24 @@
 #include <stdio.h>
 
-#include "pixmap_io.h"
+#include "nrdef.h"
+#include "nrutil.h"
 
-int width = 320;
-int height = 240;
 
-int main(){
-	char* image = load_pixmap("/home/ludovic/HPC/Projet/car3/car_3000.pgm", &width, &height);
-	store_pixmap("/home/ludovic/HPC/Projet/Code/image_test", image, width, height);
+//debut de l'algorithme de frame difference
+int main(void){
+
+	int nrl, nrh, ncl, nch;
+	int img_nbr = 0;
+	char nom_img[100];
+
+	for (img_nbr; img_nbr < 200; img_nbr++){
+		sprintf(nom_img, "/home/ludovic/HPC/Projet/car3/car_3%d.pgm", img_nbr);
+		printf("%s\n", nom_img);
+
+	}
+
+
+	// exemple de fonction pour ouverture et fermeture, attention au chemin
+	//uint8** image = LoadPGM_ui8matrix("/home/ludovic/HPC/Projet/car3/car_3000.pgm", &nrl, &nrh, &ncl, &nch);
+	//SavePGM_ui8matrix(image, nrl, nrh, ncl, nch,"/home/ludovic/HPC/Projet/Code/image_test");
 }
